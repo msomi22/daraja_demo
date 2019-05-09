272ec3a6f3941e467e707fd5a8db5f78b922a6c0
msomi22 <t_pnjeru@kcbgroup.com>
2019-05-09 16:25:44 +0300

    actuator

--- src/main/java/com/peter/demo/DarajaDemoApplication.java
+++ src/main/java/com/peter/demo/DarajaDemoApplication.java
@@ -1,8 +1,11 @@
 package com.peter.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -16,6 +19,22 @@ public class DarajaDemoApplication {
 	@Bean
 	public RestTemplate getTemplate() {
 		return new RestTemplate(); 
+	}
+	
+	@Component
+	public class CustomHealthIndicator implements HealthIndicator {
+	 
+	 @Override
+	 public Health health() {
+	  int errorCode = 0; 
+	  // In the above line,I am simple assigning zero,but you can call Health check related code like below commented line and that method can return the appropriate code.
+	  // int errorCode = performHealthCheck();
+	  if (errorCode != 0) {
+	   return Health.down().withDetail("Error Code", errorCode).build();
+	  }
+	  return Health.up().build();
+	 }
+	 
 	}
 
 }
